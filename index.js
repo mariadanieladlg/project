77b80851d5f4d3c5c5d78e6b281f1b3a4d054555
Maria Daniela Delgado <mariadanieladelgado@Marias-MacBook-Air.local>
2025-05-22 21:35:28 +0200

    Day 2

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ let parsedGem = parseFloat(gem.innerHTML);
 
 let clickerCost = document.querySelector(".clicker-cost");
 let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+let clickerLevel = document.querySelector(".clicker-level");
+let clickerIncrease = document.querySelector(".clicker-increase");
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML);
+
+let gpc = 1;
 
 function incrementGem() {
-  parsedGem += 1;
-  gem.innerHTML = parsedGem;
+  gem.innerHTML = Math.round((parsedGem += gpc));
 }
 
 function buyClicker() {
   if (parsedGem >= parsedClickerCost) {
     parsedGem -= parsedClickerCost;
     gem.innerHTML -= parsedGem;
+
+    clickerLevel.innerHTML++;
+
+    parsedClickerIncrease = parseFloat(
+      (parsedClickerIncrease * 1.03).toFixed(2)
+    );
+    clickerIncrease.innerHTML = parsedClickerIncrease;
+    gpc += parsedClickerIncrease;
   }
 }
